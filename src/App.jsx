6304b6447e3c3c4011eb2866d50fc29cf798c1ae
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-11 00:24:37 +0200

    page Welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.scss'
+import './App.scss';
+import { PageWelcome } from './pages/Welcome';
 
 function App() {
-  return (
-    <div className="App">
-    testing
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Company Site</h1>
+			<hr/>
+			<PageWelcome />
+		</div>
+	);
 }
 
-export default App
+export default App;
