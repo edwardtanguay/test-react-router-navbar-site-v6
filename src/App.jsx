82cbd042ddfe1f4246dbc7002a3441c8ed70af02
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-11 07:59:33 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import './App.scss';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageBooks } from './pages/PageBooks';
 import { PageAbout } from './pages/PageAbout';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Personal Site</h1>
 			<hr />
-			Welcome | Books | About
+			<NavLink to="/welcome">Welcome</NavLink> |{' '}
+			<NavLink to="/books">Books</NavLink> |{' '}
+			<NavLink to="/about">About</NavLink>
 			<hr />
-			<PageWelcome />
-			<PageBooks />
-			<PageAbout />
+			<Routes>
+				<Route path="/welcome" element={<PageWelcome />} />
+				<Route path="/books" element={<PageBooks />} />
+				<Route path="/about" element={<PageAbout />} />
+				<Route path="/" element={<Navigate to="/welcome" replace />}/>
+			</Routes>
 		</div>
 	);
 }
